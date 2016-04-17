5c841c23738182276d4316fe356cbb352f9ef206
meghankelly-cartography <mkelly22@wisc.edu>
2016-04-17 16:02:10 -0500

    Base map dropdown works through one iteration
    Dropdown breaks when you repeat dropdown options.

--- main/js/main.js
+++ main/js/main.js
@@ -1,7 +1,9 @@
 var attrArray = ["countries_1715", "countries_1783", "countries_1815"];
+
 var expressed = attrArray[0]
 console.log(expressed)
 //load map
+
 window.onload = setMap();
 
 //set up map and call data
@@ -34,7 +36,7 @@ function setMap(){
     	.defer(d3.json, "data/cntry1815.topojson") //load overlay spatial data of countries
         .await(callback);
         
-function callback(error, base, overlay1, overlay2, overlay3){
+	function callback(error, base, overlay1, overlay2, overlay3){
         console.log(error);
         console.log(base);
         
@@ -59,24 +61,24 @@ function callback(error, base, overlay1, overlay2, overlay3){
             .attr("d", path);
             
         //add France regions to map
-        var countries_1783 = mapContainer.selectAll(".countries_1783")
-            .data(countriesOverlay2)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "countries_1783 " + d.properties.name;
-            })
-            .attr("d", path);
+       //  var countries_1783 = mapContainer.selectAll(".countries_1783")
+//             .data(countriesOverlay2)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "countries_1783 " + d.properties.name;
+//             })
+//             .attr("d", path);
             
         //add France regions to map
-        var countries_1815 = mapContainer.selectAll(".countries_1815")
-            .data(countriesOverlay3)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "countries_1815 " + d.properties.name;
-            })
-            .attr("d", path);    
+//         var countries_1815 = mapContainer.selectAll(".countries_1815")
+//             .data(countriesOverlay3)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "countries_1815 " + d.properties.name;
+//             })
+//             .attr("d", path);    
           
              //add Europe countries to map
         var land = mapContainer.append("path")
@@ -84,8 +86,51 @@ function callback(error, base, overlay1, overlay2, overlay3){
             .attr("class", "land")
             .attr("d", path);
 		
-		createDropdown(attrArray);
-    };
+		//dropdown change listener handler
+function changeAttribute(attribute){
+    //change the expressed attribute
+    expressed = attribute;
+    console.log(expressed);
+    if (expressed == attrArray[0]) {
+    	console.log("hi")
+    	//add France regions to map
+        var expressed = mapContainer.selectAll("." + expressed)
+            .data(countriesOverlay1)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "countries_1715 " + d.properties.name;
+            })
+            .attr("d", path);
+    	}
+    else if (expressed == attrArray[1]) {
+    	console.log("hi")
+    	//add France regions to map
+        var expressed = mapContainer.selectAll("." + expressed)
+            .data(countriesOverlay2)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "countries_1783 " + d.properties.name;
+            })
+            .attr("d", path);
+    	}
+    else {
+    	console.log("hi")
+    	var expressed = mapContainer.selectAll("." + expressed)
+            .data(countriesOverlay3)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "countries_1815 " + d.properties.name;
+            })
+            .attr("d", path);    
+    	
+    	}
+    	
+        
+};
+
 
 //function to create a dropdown menu for attribute selection
 function createDropdown(attrArray){
@@ -111,13 +156,51 @@ function createDropdown(attrArray){
         .attr("value", function(d){ return d })
         .text(function(d){ return d });
 };
-
-//dropdown change listener handler
-function changeAttribute(attribute){
-    //change the expressed attribute
-    expressed = attribute;
-    console.log(expressed);
-        
+		
+		createDropdown(attrArray);
+		 
+	};
 };
 
-};
+
+
+
+
+
+// set_projection(new Option("Mercator", "mercator", false, false));
+// 
+// function set_projection(option) {
+//   proj = option.value
+// 
+//   projection = eval("d3.geo."+proj+"();");
+// 
+//   path = d3.geo.path()
+//     .projection(projection);
+// 
+//   svg.countries_1715.selectAll("path").transition()
+//       .duration(1000)
+//        .attr("d", path);
+// }
+// 
+// function projection_selected(e){
+//   set_projection(e.target[e.target.selectedIndex])
+// }
+// 
+// var newSelect = document.createElement("select");
+// newSelect.id = "selectlistid"; // add some attributes
+// newSelect.onchange = projection_selected; // call the somethingChanged function when a change is made
+// newSelect[newSelect.length] = new Option("Aitoff","aitoff",false, false)
+// newSelect[newSelect.length] = new Option("Albers equal-area conic","albers",false, false);
+// newSelect[newSelect.length] = new Option("Armadillo","armadillo",false, false);
+// newSelect[newSelect.length] = new Option("August conformal","august",false, false);
+// newSelect[newSelect.length] = new Option("Lambert azimuthal equal-area","azimuthalEqualArea",false, false);
+// newSelect[newSelect.length] = new Option("azimuthal equidistant","azimuthalEquidistant",false, false);
+// newSelect[newSelect.length] = new Option("Baker Dinomic","baker",false, false);
+// newSelect[newSelect.length] = new Option("Berghaus Star","berghaus",false, false);
+// newSelect[newSelect.length] = new Option("Boggs eumorphic","boggs",false, false);
+// newSelect[newSelect.length] = new Option("Bonne","bonne",false, false);
+// 
+// newSelect[newSelect.length] = new Option("Mercator","mercator",false, true);
+// 
+// //document.getElementById('myDiv').appendChild(newSelect); // myDiv is the container to hold the select list
+// 
