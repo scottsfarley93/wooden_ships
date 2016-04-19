b7d87658fb1bd19e3ee6678bd487564a8cf7e0c1
meghankelly-cartography <mkelly22@wisc.edu>
2016-04-19 07:27:31 -0500

    Constrained zoom.
    Zoom is constrained on Line 48. The default zoom level is 1 and it’s
    currently constrained at zoom level 2. Zoom doesn’t work in Chrome
    given a translation error on line 94. Error reads: cannot read property
    ‘translate’ of null

--- main/js/main.js
+++ main/js/main.js
@@ -26,7 +26,9 @@ $(document).ready(function(){
 	//stuff that happens as the map is created.
 	setMap(); //creates the map
 	createDropdown(attrArray); //creates the dropdown menu for years/basemap
-	projDropdown(attrProj) //creates the dropdown menu for projection
+	projDropdown(attrProj); //creates the dropdown menu for projection
+    zoomed(); //allows map to zoom
+    createSlider() //creates temporal slider
 })
 
 
@@ -43,8 +45,10 @@ function setMap(){
         var zoom = d3.behavior.zoom()
             .translate([0, 0])
             .scale(1)
-            .scaleExtent([1, 8])
+            .scaleExtent([1, 2]) //change scale here (default is 1 constrained at zoom level 2)
             .on("zoom", zoomed);
+
+            console.log(zoom.scale());
 	        
 	    //use queue.js to parallelize asynchronous data loading
 	    d3_queue.queue()
@@ -84,14 +88,13 @@ function setMap(){
 	         changeProjection("VDG");
 	}; //end of callback
 
-    function zoomed() {
-        mapContainer.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-        mapContainer.selectAll(".land").style("stroke-width", 1.5 / d3.event.scale + "px");
-        mapContainer.selectAll(".countries").style("stroke-width", .5 / d3.event.scale + "px");
-};
 };//end of set map
 
-
+function zoomed() {
+    mapContainer.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    mapContainer.selectAll(".land").style("stroke-width", 1.5 / d3.event.scale + "px");
+    mapContainer.selectAll(".countries").style("stroke-width", .5 / d3.event.scale + "px");
+};
 
 		
 //dropdown change listener handler
@@ -229,3 +232,106 @@ function changeProjection(projection, scale, center){
    globals.countries.transition().attr('d', path)
    globals.land.transition().attr('d', path)
 };
+
+
+
+function createSlider(){
+
+    formatDate = d3.time.format("%Y");
+
+    // parameters
+    var margin = {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
+      },
+      width = 400 - margin.left - margin.right,
+      height = 100 - margin.bottom - margin.top;
+
+
+    // scale function
+    var timeScale = d3.time.scale()
+      .domain([new Date('1700-01-02'), new Date('1851-01-01')])
+      .range([0, width])
+      .clamp(true);
+
+
+    // initial value
+    var startValue = timeScale(new Date('1700-03-20'));
+    startingValue = new Date('1700-03-20');
+
+    //////////
+
+    // defines brush
+    var brush = d3.svg.brush()
+      .x(timeScale)
+      .extent([startingValue, startingValue])
+      .on("brush", brushed);
+
+    var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      // classic transform to position g
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("g")
+      .attr("class", "x axis")
+    // put in middle of screen
+    .attr("transform", "translate(0," + height / 2 + ")")
+    // inroduce axis
+    .call(d3.svg.axis()
+      .scale(timeScale)
+      .orient("bottom")
+      .tickFormat(function(d) {
+        return formatDate(d);
+      })
+      .tickSize(0)
+      .tickPadding(12)
+      .tickValues([timeScale.domain()[0], timeScale.domain()[1]]))
+      .select(".domain")
+      .select(function() {
+        console.log(this);
+        return this.parentNode.appendChild(this.cloneNode(true));
+      })
+      .attr("class", "halo");
+
+    var slider = svg.append("g")
+      .attr("class", "slider")
+      .call(brush);
+
+    slider.selectAll(".extent,.resize")
+      .remove();
+
+    slider.select(".background")
+      .attr("height", height);
+
+    var handle = slider.append("g")
+      .attr("class", "handle")
+
+    handle.append("path")
+      .attr("transform", "translate(0," + height / 2 + ")")
+      .attr("d", "M 0 -20 V 20")
+
+    handle.append('text')
+      .text(startingValue)
+      .attr("transform", "translate(" + (-18) + " ," + (height / 2 - 25) + ")");
+
+    slider
+      .call(brush.event)
+
+    function brushed() {
+      var value = brush.extent()[0];
+
+      if (d3.event.sourceEvent) { // not a programmatic event
+        value = timeScale.invert(d3.mouse(this)[0]);
+        brush.extent([value, value]);
+      }
+
+      handle.attr("transform", "translate(" + timeScale(value) + ",0)");
+      handle.select('text').text(formatDate(value));
+    }
+};
+
+
